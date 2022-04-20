e50b6cd3b8bc3a7e7a37636f6b8a0369423f70f1
nathanpoirierisgreat <100322123+nathanpoirierisgreat@users.noreply.github.com>
2022-04-20 16:59:50 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,34 @@
-radio.onReceivedValue(function (name, value) {
-    let controled: number[] = []
-    if (controled[4]) {
-        cuteBot.turnleft()
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "left") {
+        cuteBot.motors(30, 50)
+    }
+    if (receivedString == "stop") {
+        cuteBot.stopcar()
+    }
+    if (receivedString == "right") {
+        cuteBot.motors(50, 30)
+    }
+    if (receivedString == "foreword") {
+        cuteBot.forward()
+    }
+    if (receivedString == "back") {
+        cuteBot.backforward()
+    }
+    if (receivedString == "honk") {
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . # . # .
+                . # . # .
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+            music.setVolume(255)
+            music.playTone(440, music.beat(BeatFraction.Whole))
+            music.playTone(0, music.beat(BeatFraction.Half))
+            music.playTone(440, music.beat(BeatFraction.Whole))
+            basic.clearScreen()
+        }
     }
 })
-radio.setGroup(60)
+radio.setGroup(255)
